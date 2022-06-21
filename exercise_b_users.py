bbbd6daa74c28f67f8c77119c904643d03a59c80
CarlyCoding <carly_off@outlook.com>
2022-06-21 21:48:55 +0100

    b9 completed

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -82,7 +82,11 @@ users["Erik"]["lottery_numbers"].append(7)
 print(users["Erik"]["lottery_numbers"])
 
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+print(users["Erik"]["home_town"])
 
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append({"name": "Fluffy", "species": "dog"})
+print(users["Erik"]["pets"])
 
 # 10. Add another person to the users dictionary
